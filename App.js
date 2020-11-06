5255dba2e15f4cc18c30e664cbc3301c4d0198da
shcheuk <shcheuklogin@gmail.com>
2020-11-06 22:07:10 +0800

    my second commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello from Lawrence CircleCI!</Text>
+        <Text>Hello from Lawrence 2 CircleCI!</Text>
       </View>
     );
   }
