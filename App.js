ca9df13c9acf1030ba23f6abbb072a57dbfc2575
shcheuk <shcheuklogin@gmail.com>
2020-11-14 00:00:42 +0800

    Revert "my first trial" "revert my first trial"
    This reverts commit bf857c877ceae1c630de9d9db7d8053fd8874256.

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello from Lawrence CircleCI!</Text>
+        <Text>Hello from CircleCI!</Text>
       </View>
     );
   }
